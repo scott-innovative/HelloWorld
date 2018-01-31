9d0e614c64fca3d9aa576158cdd27b25a5a425d7
Scott <scott@knowthefacts.com>
2018-01-31 16:04:34 -0500

    added class

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -4,9 +4,16 @@ namespace HelloWorld
 {
     class Program
     {
+        class Person
+        {
+            public string Name { get; set; }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Person employee = new Person();
+            employee.Name = "Scott Walker";
         }
     }
 }
