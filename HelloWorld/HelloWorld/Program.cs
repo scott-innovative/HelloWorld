cd335bf48aa77fd5e5463ee074d3e682c986f609
Scott <scott@knowthefacts.com>
2018-01-31 16:21:15 -0500

    added manager class

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -8,12 +8,18 @@ namespace HelloWorld
         {
             public string Name { get; set; }
         }
+        class Manager
+        {
+            public string Name { get; set; }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
             Person employee = new Person();
             employee.Name = "Scott Walker";
+            Manager manager = new Manager();
+            manager.Name = "John Smith";
         }
     }
 }
